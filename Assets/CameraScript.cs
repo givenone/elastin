65211326b95d4bab455082672ab24db5afc84de7
Yong-Hwan Yoo <ajoiej729@gmail.com>
2020-09-11 14:23:14 +0900

    camera script in progress

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System.IO;
+
+public class CameraScript : MonoBehaviour
+{
+    WebCamTexture webcamTexture;
+    RawImage rawImage;
+    string path = "Assets/demo_files/";
+    void Start()
+    {
+        webcamTexture = new WebCamTexture();
+        rawImage = GetComponent<RawImage>();
+        rawImage.texture = webcamTexture;
+        rawImage.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+    public void TakePhoto(){
+
+        // yield return new WaitForEndOfFrame ();
+
+        var width = 320;
+        var height = 240;
+
+        Texture2D texture = new Texture2D(width, height);
+        texture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        texture.Apply();
+
+        // Encode texture into PNG
+        var bytes = texture.EncodeToPNG();
+        //Destroy(texture);
+
+
+        File.WriteAllBytes (path + "SavedScreen.png", bytes);
+
+    }
+}
