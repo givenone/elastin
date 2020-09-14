cc6484462d75087ed88814acdc78a70d9dca4718
Yong-Hwan Yoo <ajoiej729@gmail.com>
2020-09-15 01:17:56 +0900

    device cam script

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -9,31 +9,40 @@ public class CameraScript : MonoBehaviour
     WebCamTexture webcamTexture;
     RawImage rawImage;
     string path = "Assets/demo_files/";
-    void Start()
+    public void Start()
     {
-        webcamTexture = new WebCamTexture();
-        rawImage = GetComponent<RawImage>();
-        rawImage.texture = webcamTexture;
-        rawImage.material.mainTexture = webcamTexture;
-        webcamTexture.Play();
+        if (rawImage == null || !rawImage.enabled ) {
+            webcamTexture = new WebCamTexture();
+            rawImage = GetComponent<RawImage>();
+            rawImage.enabled = true;
+            rawImage.texture = webcamTexture;
+            rawImage.material.mainTexture = webcamTexture;
+            webcamTexture.Play();
+        }
     }
-    public void TakePhoto(){
-
-        // yield return new WaitForEndOfFrame ();
-
-        var width = 320;
-        var height = 240;
-
-        Texture2D texture = new Texture2D(width, height);
-        texture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
-        texture.Apply();
-
-        // Encode texture into PNG
-        var bytes = texture.EncodeToPNG();
-        //Destroy(texture);
-
-
-        File.WriteAllBytes (path + "SavedScreen.png", bytes);
-
+    void Update()
+     {
+         //This is to take the picture, save it and stop capturing the camera image.
+         if(Input.GetMouseButtonDown(0) & rawImage.enabled)
+         {
+             SaveImage();
+             webcamTexture.Stop();
+             rawImage.enabled = false;
+         }
+     }
+    void SaveImage()
+    {
+         //Create a Texture2D with the size of the rendered image on the screen.
+         Texture2D texture = new Texture2D(rawImage.texture.width, rawImage.texture.height, TextureFormat.ARGB32, false);
+         
+         //Save the image to the Texture2D
+         texture.SetPixels(webcamTexture.GetPixels());
+         texture.Apply();
+ 
+         //Encode it as a PNG.
+         byte[] bytes = texture.EncodeToPNG();
+         
+         //Save it in a file.
+         File.WriteAllBytes(path + "SavedScreen.png", bytes);
     }
 }
